ce053abf2686e2d6ebcf958071ff957cd197e64c
Nippel69 <nephro13@googlemail.com>
2014-06-17 13:57:51 +0200

    Update Main.java

--- roboter/src/roboter/Main.java
+++ roboter/src/roboter/Main.java
@@ -1,26 +1,18 @@
 package roboter;
 
-import image.CommandRaspberryPi;
-
 import java.rmi.RemoteException;
 
 public class Main {
+	private static Calibrate cali;
+	private static NewDriver drive;
 
-    private static Calibrate cali;
-    private static NewDriver drive;
-    private static CommandRaspberryPi cmdRsPi;
+	public static void main(String[] args) throws InterruptedException,
+			RemoteException {
 
-    public static void main(String[] args) throws InterruptedException, RemoteException {
-        cmdRsPi = new CommandRaspberryPi();
-        if (cmdRsPi.connectToEV3()) {
-            System.out.print("Connecting...");
-            Remote remote = new Remote();
-            remote.remote();
-            remote.testDrive();
-        }else{
-            
-        }
+		cali = new Calibrate();
+		drive = new NewDriver(cali);
+		drive.drive();
 
-    }
+	}
 
 }
